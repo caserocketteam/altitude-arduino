338cd17016404f06ab489f7331b511c99d21f911
pfeeley <pgf8@case.edu>
2015-03-21 00:15:45 -0400

    Add sensor libraries

--- altitude/altitude.ino
+++ altitude/altitude.ino
@@ -1,4 +1,9 @@
 #include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_LSM303_U.h>
+#include <Adafruit_BMP085_U.h>
+#include <Adafruit_L3GD20_U.h>
+#include <Adafruit_10DOF.h>
 
 //timing values
 float time;
