95d93dd3883d0f91f136bae54118118c3d0d38db
pfeeley <pgf8@case.edu>
2015-03-21 10:15:45 -0400

    Update sensors to get relevant data

--- altitude/altitude.ino
+++ altitude/altitude.ino
@@ -12,8 +12,12 @@ Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
 Adafruit_BMP085_Unified       bmp   = Adafruit_BMP085_Unified(18001);
 
 //timing values
-float time;
-float last_time;
+unsigned long time = 0;
+unsigned long last_time = 0;
+unsigned long time_difference;
+int target_altitude = 1625;
+int launch_altitude = 400;  // ** SET THIS BEFORE LAUNCH **
+int absolute_distance = target_altitude + launch_altitude;
 
 //declare pin aliases
 int solenoid = 13;
@@ -21,7 +25,10 @@ int sensors = 0;
 
 //control values
 bool brake = false;
-float seaLevelPressure = 1016;
+float seaLevelPressure = 1016;  // ** SET THIS BEFORE LAUNCH **
+float altitude;
+float last_altitude;
+float altitude_change;
 
 void setup() {
   pinMode(solenoid, OUTPUT);
@@ -29,42 +36,18 @@ void setup() {
 }
 
 void loop() {
-    //TODO: convert serial out to send over xbee
   sensors_event_t accel_event;
-  sensors_event_t mag_event;
   sensors_event_t bmp_event;
-  sensors_vec_t   orientation;
-  
-  accel.getEvent(&accel_event);
-  mag.getEvent(&mag_event);
-  
-  if (dof.fusionGetOrientation(&accel_event, &mag_event, &orientation)) {
-    /* 'orientation' should have valid .roll and .pitch fields */
-    Serial.print(F("Orientation: "));
-    Serial.print(orientation.roll);
-    Serial.print(F(" "));
-    Serial.print(orientation.pitch);
-    Serial.print(F(" "));
-    Serial.print(orientation.heading);
-    Serial.println(F(""));
-  }
-  
+
   /* Calculate the altitude using the barometric pressure sensor */
   bmp.getEvent(&bmp_event);
   if (bmp_event.pressure){
     /* Get ambient temperature in C */
     float temperature;
     bmp.getTemperature(&temperature);
+    
     /* Convert atmospheric pressure, SLP and temp to altitude */
-    Serial.print(F("Alt: "));
-    Serial.print(bmp.pressureToAltitude(seaLevelPressure,
-                                        bmp_event.pressure,
-                                        temperature)); 
-    Serial.println(F(""));
-    /* Display the temperature */
-    Serial.print(F("Temp: "));
-    Serial.print(temperature);
-    Serial.println(F(""));
+    altitude = bmp.pressureToAltitude(seaLevelPressure, bmp_event.pressure, temperature); 
   }
     
   Wire.requestFrom(2,7);  //check I2C bit length
@@ -74,7 +57,21 @@ void loop() {
     Serial.print(c);
   }
   
-  //math goes here
+  //calculate speed
+  if(last_time == 0){
+    last_time = millis();
+  } else {
+    time = millis();
+    time_difference = time - last_time;
+    accel.getEvent(&accel_event);
+    
+    last_time = time;
+  }
+  
+  //calculate change in altitude
+  altitude_change = altitude - last_altitude;
+  
+  //math for control goes here
   
   if(brake == false){
     digitalWrite(solenoid, LOW);
